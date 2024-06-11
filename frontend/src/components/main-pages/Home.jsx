1169e3903c21fa9fdcb338b2602bf0ec8d0d14ce
devindawanasinghe <devindawanasinghe@gmail.com>
2024-06-11 14:12:02 +0530

    update components

--- frontend/src/components/main-pages/Home.jsx
+++ frontend/src/components/main-pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './../../css/home.css';
+import homebgimage from "../../images/Hbg.jpg";
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='wrapper'>
+      <div className='main'>
+
+
+
+
+
+
+      </div>
+    </div>
   )
 }
 
