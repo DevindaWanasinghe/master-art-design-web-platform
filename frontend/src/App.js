a79382d4d8d08445c39501ccb2c0f223d9b94f1b
devindawanasinghe <devindawanasinghe@gmail.com>
2024-06-16 14:26:52 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import Home from './components/main-pages/Home';
+import Account from './components/main-pages/Account';
+import Template from './components/main-pages/Template';
+import{ BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
+  
+   <div> 
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/home" element={<Home/>}></Route>
+          <Route path = "/account" element={<Account/>}></Route>
+          <Route path = "/template" element={<Template/>}></Route>
 
+        </Routes>
+
+      </BrowserRouter>
 
    </div>
   );
@@ -11,3 +25,4 @@ function App() {
 
 
 export default App;
+
