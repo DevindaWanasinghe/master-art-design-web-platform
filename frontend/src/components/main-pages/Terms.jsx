5febb5f2589bf2ba47c715387cefa18017c151c1
devindawanasinghe <devindawanasinghe@gmail.com>
2024-06-24 12:59:17 +0530

    terms error update

--- frontend/src/components/main-pages/Terms.jsx
+++ frontend/src/components/main-pages/Terms.jsx
@@ -2,19 +2,14 @@ import React from "react";
 import Bimage1 from "../../images/Home/Background girl image.png";
 import Bimage2 from "../../images/Home/Background black-2.png";
 import Bimage3 from "../../images/Home/Background black-1.png";
-import Arrow01 from "../../images/Home/Arrow.png";
 import Search01 from "../../images/Home/search.png";
 import Logo01 from "../../images/Home/LOGO 1.png";
 import NavBarBackground from "../../images/Home/NavBarbackground.png";
 import TermsBack from "../../images/Home/TermsBack.png";
 
- 
-import "./../../css/terms.css"; // Import custom CSS
-import "./../../css/home.css"; // Import custom CSS
+import '../../css/terms.css'; // Import custom CSS
 
-<script src="https://cdn.tailwindcss.com"></script>;
-
-function terms() {
+function Terms() {
   return (
     <div className="relative w-screen h-screen overflow-hidden">
       {/* Background Image */}
@@ -39,11 +34,10 @@ function terms() {
       />
 
       {/* Text Overlay */}
-
       <div>
-        <boutton className="button02C">log in</boutton>
-        <boutton className="button03C">sign in</boutton>
-        <boutton className="button04C">search...</boutton>
+        <button className="button02C">log in</button>
+        <button className="button03C">sign in</button>
+        <button className="button04C">search...</button>
         <img src={Search01} alt="Arrow" className="search01C" />
       </div>
       <div>
@@ -51,17 +45,17 @@ function terms() {
         <img src={NavBarBackground} alt="NavBarBackground" className="navbarback01C"/>
       </div>
       
-      <div calssname="termsback01C">
+      <div className="termstext">
         <img
-        src={TermsBack}
-        alt="Overlay image background"
-        className="absolute inset-0 object-cover w-full h-full"
+          src={TermsBack}
+          alt="Overlay image background"
+          className="absolute inset-0 object-cover w-full h-full"
         />
       </div>
       
       <div className="absolute text02C" style={{ left: "200px", top: "183.62px" }}>
-        <h5 classname="termstext01C">MasterArt Template Store - Terms and Conditions</h5>
-        <p classname="termstext02C">
+        <h5 className="termstext">MasterArt Template Store - Terms and Conditions</h5>
+        <p className="termstext02C">
           Welcome to MasterArt Template Store. By accessing and using our website,
           you agree to comply with and be bound by the following terms and
           conditions. Please read them carefully.
@@ -83,13 +77,13 @@ function terms() {
           collected during the purchase process is kept confidential and used solely
           for order processing and communication. For more details, please review
           our Privacy Policy.
-          </p>
-          <div>
-        <boutton className="termsbutton01C">cloase</boutton>
-      </div>
+        </p>
+        <div>
+          <button className="termsbutton01C">close</button>
+        </div>
       </div>
     </div>
   );
 }
 
-export default terms;
+export default Terms;
