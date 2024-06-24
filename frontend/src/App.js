8f8f9cc2d1540d409601f925bcf1fea4da532ba2
IsharaWanninayaka <isharawanninayaka26@gmail.com>
2024-06-24 12:42:02 +0530

    add-registration-form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Home from './components/main-pages/Home';
 import Account from './components/main-pages/Account';
 import Template from './components/main-pages/Template';
+import Register from './components/login-pages/Register';
+import Login from './components/login-pages/Login';
 import{ BrowserRouter, Routes, Route } from "react-router-dom";
 
 
@@ -14,6 +16,8 @@ function App() {
           <Route path = "/home" element={<Home/>}></Route>
           <Route path = "/account" element={<Account/>}></Route>
           <Route path = "/template" element={<Template/>}></Route>
+          <Route path = "/register" element={<Register/>}></Route>
+          <Route path = "/login" element={<Login/>}></Route>
 
         </Routes>
 
