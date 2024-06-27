2d6c6ff9b9b68a2deaa8c329a80e72bb872b08a0
avishka28 <avishkabandara119@gmail.com>
2024-06-27 13:32:44 +0530

    update

--- frontend/src/components/main-pages/Account.jsx
+++ frontend/src/components/main-pages/Account.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import BgImage from '../common/Bgimage'
+import AcImg from  '../../images/AccountImg/accover.png'
+
+
+
 
 function Account() {
   return (
-    <div>Account</div>
+    <div className=' relative min-h-screen'>
+      <BgImage/>
+      <div className=' border-2 w-[1137px] h-[333px] text-white bg- relative z-10 pt-16  mt-[200px]'>
+
+            <div>
+              <AcImg/>
+              <p className=' text-black'>Notification cente </p>
+            </div>
+            <div>
+
+
+
+
+
+
+
+
+            </div>
+
+            <div>
+
+
+
+
+
+
+            </div>
+      </div>
+
+
+    </div>
   )
 }
 
