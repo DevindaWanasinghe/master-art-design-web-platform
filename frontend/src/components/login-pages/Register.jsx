ec5d106e55b4a4b70a56566b54d1ecb971857202
IsharaWanninayaka <isharawanninayaka26@gmail.com>
2024-06-30 21:38:17 +0530

    update google login

--- frontend/src/components/login-pages/Register.jsx
+++ frontend/src/components/login-pages/Register.jsx
@@ -4,7 +4,8 @@ import Header from "../main-pages/Header";
 import Validation from '../../script/registervalidation';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-
+import { FcGoogle } from "react-icons/fc"; 
+import { useGoogleLogin } from '@react-oauth/google';
 
 function Login() {
     const [InputValue,setInputValue] = useState({
@@ -12,7 +13,6 @@ function Login() {
       email:"",
       phone:"",
       password:"",
-      repeatePassword:""
 
     });
 
@@ -20,11 +20,16 @@ function Login() {
     const [errors,seterrors] =useState({});
     const navigate = useNavigate();
 
+    const login = useGoogleLogin({
+      onSuccess: credentialResponse => console.log(credentialResponse),
+      onError: console.log("Login Failed")
+    });
+
     const handelSubmit=(e)=>{
       e.preventDefault();
       seterrors(Validation(InputValue));
      
-      if(errors.userName === "" && errors.email === "" && errors.password ==="" && errors.phone === "" && errors.repeatePassword === ""){
+      if(errors.userName === "" && errors.email === "" && errors.password ==="" && errors.phone === ""){
         setloading(true);
         axios.post('http://localhost:8081/user/register',InputValue)
           .then(res =>{
@@ -57,11 +62,11 @@ function Login() {
             <span className=' text-red-700 text-[10px] '><p className='text-center'>{errors.phone}</p></span>
             <input onChange={e => setInputValue({...InputValue,password:e.target.value})} className='w-5/6 h-[45px] bg-transparent border-[1px] border-white rounded-[14px] mt-[15px]  text-white  placeholder-white focus:outline-0 p-4 lg:ml-[25px] md:ml-[15px]'placeholder='Password' type='password' name='Password'></input>
             <span className=' text-red-700 text-[10px] '><p className='text-center'>{errors.password}</p></span>
-            <input onChange={e => setInputValue({...InputValue,repeatePassword:e.target.value})} className='w-5/6 h-[45px] bg-transparent border-[1px] border-white rounded-[14px] mt-[15px]  text-white  placeholder-white focus:outline-0 p-4 lg:ml-[25px] md:ml-[15px]'placeholder='Password' type='password' name='repeatePassword'></input>
-            <span className=' text-red-700 text-[10px] '><p className='text-center'>{errors.repeatePassword}</p></span>
-            <button className='w-5/6 h-[45px] bg-#9573A9 border-[1px] rounded-[14px] mt-[30px] ml-[24px] text-white bg-custom-purple font-bold text-[20px] border-none  hover:bg-blue-700 mb-[34px]' type='submit' >
+            <button className='w-5/6 h-[45px] bg-#9573A9 border-[1px] rounded-[14px] mt-[20px] ml-[24px] text-white bg-custom-purple font-bold text-[20px] border-none  hover:bg-blue-700 mb-[18px]' type='submit' >
             {loading ? <span className=''><div class="w-8 h-8 border-2 border-dashed rounded-full animate-spin border-white ml-[17px] mt-[-3px]"></div><p  className='ml-[1px] mt-[-34px]'>loading...</p></span> :<p className='pb-[4px] '>Continue</p>}
             </button>
+            <p className='text-center text-white text-[15px] mt-[-18px]'>or</p>
+            <button onClick={()=>{login()}} className='w-5/6 h-[45px] bg-#9573A9 border-[1px] rounded-[14px] mt-[5px] ml-[24px] text-white font-bold text-[15px]  hover:bg-blue-700  border-white flex justify-center mb-[23px]'><FcGoogle size={20} className=' ml-[2px] mt-[12px]' /><p className='mt-[10px] ml-[2px]'>Continue With Google</p></button>
         </form>
             
       </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="297432764501-dt8ik6e51lqbrgm9rt5o5ldcjptub5p2.apps.googleusercontent.com"> <App /></GoogleOAuthProvider>;
   </React.StrictMode>
 );
 
