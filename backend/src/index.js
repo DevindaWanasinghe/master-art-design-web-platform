c4da1aebeb09850789c1cd66c04ef1dc22bc7c40
devindawanasinghe <devindawanasinghe@gmail.com>
2024-06-30 21:15:20 +0530

    index.js remove

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const routes = require('../src/routs/routes');
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const routes = require('../src/routs/routes');
 
-const app = express();
-const port = process.env.PORT || 5000;
+// const app = express();
+// const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use('/api', routes);
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use('/api', routes);
 
-// Define a route for the root URL
-app.get('/', (req, res) => {
-    res.send('Welcome to the API!');
-  });
+// // Define a route for the root URL
+// app.get('/', (req, res) => {
+//     res.send('Welcome to the API!');
+//   });
 
-// app.listen(port, () => {
-//     console.log(`Server is running on port ${port}`);
-// });
+// // app.listen(port, () => {
+// //     console.log(`Server is running on port ${port}`);
+// // });
