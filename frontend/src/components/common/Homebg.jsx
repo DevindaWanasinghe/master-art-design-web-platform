524413c6a6493a767d00fed47d238206e8d71e34
devindawanasinghe <devindawanasinghe@gmail.com>
2024-07-01 10:56:34 +0530

    terms page update

--- frontend/src/components/common/Homebg.jsx
+++ frontend/src/components/common/Homebg.jsx
@@ -1,12 +1,12 @@
 import React,{useState} from 'react'
-import tempbgimg from '../../images/Home/Background girl image.png';
+import tempbgimg2 from '../../images/Home/Background girl image.png';
 import '../../../src/index.css';
-import '../../css/template.css';
+//import '../../css/template.css';
 
 const Bgimage = () => {
   return (
     <div className="fixed top-0 left-0 z-0 w-full h-full">
-    <img src={tempbgimg} alt='background' className='object-cover w-full h-full'/>
+    <img src={tempbgimg2} alt='background' className='object-cover w-full h-full'/>
   </div>
 );
 }
--- frontend/src/components/main-pages/Terms.jsx
+++ frontend/src/components/main-pages/Terms.jsx
@@ -1,87 +1,67 @@
 import React from "react";
-import Bimage1 from "../../images/Home/Background girl image.png";
-import Bimage2 from "../../images/Home/Background black-2.png";
-import Bimage3 from "../../images/Home/Background black-1.png";
-import Search01 from "../../images/Home/search.png";
-import Logo01 from "../../images/Home/LOGO 1.png";
-import NavBarBackground from "../../images/Home/NavBarbackground.png";
-import TermsBack from "../../images/Home/TermsBack.png";
+//import TermsBg from "../../images/Home/termsbg.png"
+import HomeBg from "../common/Homebg"
+import SideBar from "../main-pages/Sidebar"
+import Header from "../main-pages/Header"
+import { Link } from "react-router-dom";
 
-import '../../css/terms.css'; // Import custom CSS
 
 function Terms() {
   return (
-    <div className="relative w-screen h-screen overflow-hidden">
-      {/* Background Image */}
-      <img 
-        src={Bimage1}
-        alt="Overlay image1"
-        className="absolute inset-0 object-cover w-full h-full"
-      />
+    <div className="">
+      <div><HomeBg/></div>
+      <div><Header/></div>
+      <div><SideBar/></div>
 
-      {/* Overlay Image 1 */}
-      <img
-        src={Bimage2}
-        alt="Overlay image2"
-        className="absolute inset-0 object-cover w-full h-full"
-      />
-
-      {/* Overlay Image 2 */}
-      <img
-        src={Bimage3}
-        alt="Overlay image3"
-        className="absolute inset-0 object-cover w-full h-full"
-      />
-
-      {/* Text Overlay */}
-      <div>
-        <button className="button02C">log in</button>
-        <button className="button03C">sign in</button>
-        <button className="button04C">search...</button>
-        <img src={Search01} alt="Arrow" className="search01C" />
+      <div className=" pt-[109px]"> 
+        <div className="border-2 lg:w-[770px] lg:h-[681px] backdrop-blur-xl rounded-[33px] m-auto p-12 md:w-[600px]  md:h-[500px]">
+  
+            <div className="text-white lg:text-[18px] md:text-[14px]">
+              <p className="m-auto text-center mt-[-10px] pb-[25px] lg:text-[25px] font-medium md:text-[20px]">MasterArt Template Store - Terms and Conditions</p>
+              <p className="pb-2 ">
+                  Welcome to MasterArt Template Store. By accessing and using our website,
+                  you agree to comply with and be bound by the following terms and
+                  conditions. Please read them carefully.
+              </p>
+              <p className="pb-2 ">
+                  We offer a guaranteed delivery time of 5 hours for all template purchases.
+                  Delivery times may vary based on order volume and other factors.
+              </p>
+              <p className="pb-2 ">
+                  All our design templates are priced affordably. Prices are subject to change
+                  without prior notice. The price at the time of purchase is final.
+              </p>
+              <p className="pb-2">
+                  Templates purchased from MasterArt Template Store are for personal and
+                  professional use only. Redistribution, resale, or sharing of templates is
+                  strictly prohibited without prior written consent from MasterArt.
+              </p>
+              <p className="pb-2">
+                  Our templates are designed for easy customization. If you encounter any
+                  issues with customization, please contact our support team for assistance.
+              </p>
+              <p className="pb-2">
+                  We are committed to protecting your privacy. Personal information
+                  collected during the purchase process is kept confidential and used solely
+                  for order processing and communication. For more details, please review
+                  our Privacy Policy.
+              </p>
+            </div>
+          
+            <div className="flex justify-center lg:mt-[30px] md:mt-[-25px]">
+              <Link to='/'>
+              <button className="w-[151px] h-[38px] text-center border-2 rounded-[188px] bg-[#9573A9] hover:bg-[#6b427c] transition duration-300 text-white text-[20px] font-medium">
+                close
+              </button>
+              </Link>
+            </div>
       </div>
-      <div>
-        <img src={Logo01} alt="Logo" className="logo02C" />
-        <img src={NavBarBackground} alt="NavBarBackground" className="navbarback01C"/>
+
       </div>
+
+  
+
       
-      <div className="termstext">
-        <img
-          src={TermsBack}
-          alt="Overlay image background"
-          className="absolute inset-0 object-cover w-full h-full"
-        />
-      </div>
-      
-      <div className="absolute text02C" style={{ left: "200px", top: "183.62px" }}>
-        <h5 className="termstext">MasterArt Template Store - Terms and Conditions</h5>
-        <p className="termstext02C">
-          Welcome to MasterArt Template Store. By accessing and using our website,
-          you agree to comply with and be bound by the following terms and
-          conditions. Please read them carefully.
-
-          We offer a guaranteed delivery time of 5 hours for all template purchases.
-          Delivery times may vary based on order volume and other factors.
-
-          All our design templates are priced affordably. Prices are subject to change
-          without prior notice. The price at the time of purchase is final.
-
-          Templates purchased from MasterArt Template Store are for personal and
-          professional use only. Redistribution, resale, or sharing of templates is
-          strictly prohibited without prior written consent from MasterArt.
-
-          Our templates are designed for easy customization. If you encounter any
-          issues with customization, please contact our support team for assistance.
-
-          We are committed to protecting your privacy. Personal information
-          collected during the purchase process is kept confidential and used solely
-          for order processing and communication. For more details, please review
-          our Privacy Policy.
-        </p>
-        <div>
-          <button className="termsbutton01C">close</button>
-        </div>
-      </div>
     </div>
   );
 }
